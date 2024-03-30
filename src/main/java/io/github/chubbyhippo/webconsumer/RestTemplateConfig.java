5126b77f893ab7032063f7c7e086fa81c6747941
chubbyhippo <mkmsacc@outlook.com>
2024-03-30 22:22:36 +0700

    Add RestTemplateConfig class
    This commit adds a new class, RestTemplateConfig, responsible for setting up the RestTemplate bean. It sets up SSL settings and base URI, enabling interactions with secure resources.

---
+++ src/main/java/io/github/chubbyhippo/webconsumer/RestTemplateConfig.java
@@ -0,0 +1,18 @@
+package io.github.chubbyhippo.webconsumer;
+
+import org.springframework.boot.ssl.SslBundle;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder, SslBundles sslBundles) {
+        SslBundle sslBundle = sslBundles.getBundle("wiremock");
+        return builder.rootUri("https://localhost:9443").setSslBundle(sslBundle).build();
+    }
+}
