398b5f5f86181ba4c2a70161e2fd528d98fddff0
chubbyhippo <mkmsacc@outlook.com>
2024-03-31 10:31:32 +0700

    Introduce configurable port in RestTemplateConfig
    The '@Value' annotation from Spring framework was added to import the 'hello' service's port from configuration. The rootUri in RestTemplate

--- src/main/java/io/github/chubbyhippo/webconsumer/RestTemplateConfig.java
+++ src/main/java/io/github/chubbyhippo/webconsumer/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package io.github.chubbyhippo.webconsumer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ssl.SslBundle;
 import org.springframework.boot.ssl.SslBundles;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -10,9 +11,12 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
 
+    @Value("${hello.port}")
+    private int port;
+
     @Bean
     RestTemplate restTemplate(RestTemplateBuilder builder, SslBundles sslBundles) {
         SslBundle sslBundle = sslBundles.getBundle("my-bundle");
-        return builder.rootUri("https://localhost:9443").setSslBundle(sslBundle).build();
+        return builder.rootUri("https://localhost:" + port).setSslBundle(sslBundle).build();
     }
 }
