af05da64b9ef1bcd1f1e13a0e033232da6ec3436
chubbyhippo <mkmsacc@outlook.com>
2024-03-31 10:46:42 +0700

    Refactor WebConsumerApplicationTests for enhanced testing
    Modified the WebConsumerApplicationTests class to use SpringBootTest.WebEnvironment.RANDOM_PORT and added a test to validate that "/hello" endpoint returns "Hello!". These changes make tests more thorough and representative of the application's real behavior, providing more accurate test results.

--- src/test/java/io/github/chubbyhippo/webconsumer/WebConsumerApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/webconsumer/WebConsumerApplicationTests.java
@@ -1,13 +1,24 @@
 package io.github.chubbyhippo.webconsumer;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(ContainersConfig.class)
 class WebConsumerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    @DisplayName("should get hello when call hello")
+    void shouldGetHelloWhenCallHello(@Autowired WebTestClient webTestClient) {
+        webTestClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/hello").build())
+                .exchange()
+                .expectBody(String.class)
+                .isEqualTo("Hello!");
+    }
 
 }
