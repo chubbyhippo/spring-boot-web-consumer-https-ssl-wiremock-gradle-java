e82bf2d7b0d2ce79222a51618343375bbe8ab5bc
ch <mkmsacc@outlook.com>
2025-05-15 09:12:18 +0700

    Refactor ContainersConfig to separate property registration.
    Decoupled DynamicPropertyRegistry logic from the ComposeContainer bean by introducing a dedicated DynamicPropertyRegistrar bean. Added @ServiceConnection to the ComposeContainer bean for better integration with testcontainers. This improves clarity and modularity of the configuration.

--- src/test/java/io/github/chubbyhippo/webconsumer/ContainersConfig.java
+++ src/test/java/io/github/chubbyhippo/webconsumer/ContainersConfig.java
@@ -1,9 +1,12 @@
 package io.github.chubbyhippo.webconsumer;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistrar;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.ComposeContainer;
+import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
 import java.io.File;
@@ -12,19 +15,22 @@ import java.io.File;
 public class ContainersConfig {
 
     @Bean
-    ComposeContainer composeContainer(DynamicPropertyRegistry registry) {
-
-        var compose = new ComposeContainer(new File("compose.yaml"))
+    @ServiceConnection
+    public ComposeContainer composeContainer() {
+        //noinspection resource
+        return new ComposeContainer(new File("compose.yaml"))
                 .withExposedService("wiremock",
                         9443,
                         Wait.forListeningPort())
                 .withLocalCompose(true);
-
-        registry.add("hello.host", () -> compose.getServiceHost("wiremock", 9443));
-        registry.add("hello.port", () -> compose.getServicePort("wiremock", 9443));
-
-        return compose;
     }
 
+    @Bean
+    public DynamicPropertyRegistrar dynamicPropertyRegistrar(ComposeContainer compose) {
+        return registry -> {
+            registry.add("hello.host", () -> compose.getServiceHost("wiremock", 9443));
+            registry.add("hello.port", () -> compose.getServicePort("wiremock", 9443));
+        };
+    }
 
 }
