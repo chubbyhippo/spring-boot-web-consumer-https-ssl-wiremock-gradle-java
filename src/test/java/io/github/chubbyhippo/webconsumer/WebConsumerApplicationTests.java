5eceb93028357ccb6a591349b5a4c8d491b3cd3f
ch <mkmsacc@outlook.com>
2025-05-15 09:23:28 +0700

    Refactor import to use TestcontainersConfiguration
    Replaced ContainersConfig with TestcontainersConfiguration in WebConsumerApplicationTests. This improves consistency with the updated test configuration and aligns with current naming standards.

--- src/test/java/io/github/chubbyhippo/webconsumer/WebConsumerApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/webconsumer/WebConsumerApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Import(ContainersConfig.class)
+@Import(TestcontainersConfiguration.class)
 class WebConsumerApplicationTests {
 
     @Test
