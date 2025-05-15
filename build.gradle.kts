0f8c597244c49638c22ff071996248c7c6040589
ch <mkmsacc@outlook.com>
2025-05-15 09:12:04 +0700

    Update Spring Boot and dependency-management plugin versions
    Upgraded Spring Boot to version 3.4.5 and dependency-management plugin to version 1.1.7. This ensures the project uses the latest features, improvements, and security patches.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.2.4"
-	id("io.spring.dependency-management") version "1.1.4"
+	id("org.springframework.boot") version "3.4.5"
+	id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "io.github.chubbyhippo"
