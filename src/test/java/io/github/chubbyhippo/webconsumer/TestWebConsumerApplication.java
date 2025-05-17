ddac98f027e3ef29cfb20e3dd9320b0cfcbfcf4e
chubbyhippo <mkmsacc@outlook.com>
2025-05-17 09:20:02 +0700

    `Refactor package structure of TestWebConsumerApplication`
    Moved TestWebConsumerApplication to a different package for better alignment with project structure. Removed unnecessary imports to clean up the codebase.

--- src/test/java/io/github/chubbyhippo/webconsumer/TestWebConsumerApplication.java
+++ src/test/java/io/github/chubbyhippo/webconsumer/TestWebConsumerApplication.java
@@ -1,7 +1,5 @@
-package io.github.chubbyhippo.webconsumer.client;
+package io.github.chubbyhippo.webconsumer;
 
-import io.github.chubbyhippo.webconsumer.TestcontainersConfiguration;
-import io.github.chubbyhippo.webconsumer.WebConsumerApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 
