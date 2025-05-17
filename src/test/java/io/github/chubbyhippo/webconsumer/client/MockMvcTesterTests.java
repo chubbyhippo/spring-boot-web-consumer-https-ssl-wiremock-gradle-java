4c236246256762b3bfdb39cd0ccf24fd1825e8dd
chubbyhippo <mkmsacc@outlook.com>
2025-05-17 09:27:31 +0700

    Refactor MockMvcTesterTests to remove unused imports
    Eliminated redundant imports to improve code readability and maintainability. This change cleans up the test class by removing unused MockMvc-related methods and associated static imports.

--- src/test/java/io/github/chubbyhippo/webconsumer/client/MockMvcTesterTests.java
+++ src/test/java/io/github/chubbyhippo/webconsumer/client/MockMvcTesterTests.java
@@ -7,13 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 import org.testcontainers.junit.jupiter.Testcontainers;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(TestcontainersConfiguration.class)
